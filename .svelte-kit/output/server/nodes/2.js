3d70c1f308568f5adb71d7669809f7222d57c4d4
SatStone <229725033+Sat-Stone@users.noreply.github.com>
2026-01-13 18:59:42 +0100

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BOS6xc5A.js","_app/immutable/chunks/DBc14fdn.js","_app/immutable/chunks/CpRHF_J6.js","_app/immutable/chunks/CwfX5RAp.js","_app/immutable/chunks/DFbZmBis.js","_app/immutable/chunks/BzEoO9uo.js","_app/immutable/chunks/jUeHQVEq.js","_app/immutable/chunks/B0ZUWw0k.js"];
-export const stylesheets = ["_app/immutable/assets/2.D-jm9SP1.css"];
-export const fonts = [];
